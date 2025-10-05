b8e09a3b7dde0e5a99b7889522e0f695d90559b0
Owly-dabs <owlycodes@gmail.com>
2025-10-05 23:00:02 +0800

    test: implement cli for easier function testing

---
+++ cli-fire.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+import fire
+from indexer import parse, main, search
+
+if __name__ == "__main__":
+    fire.Fire({
+        "parse": parse,
+        "index": main,
+        "search": search,
+    })
+    
